42431825efa623837f8eb217bb9ee4f5f3e1bcbe
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2020-01-13 14:08:07 +0530

    browser router for navigation blw components

--- public/src/App.js
+++ public/src/App.js
@@ -3,12 +3,12 @@ import {Route,Switch} from 'react-router-dom'
 import './App.css';
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
-import Header from './components/Header/Header'
+import HomePage from './components/HomePage/HomePage'
 
 function App() {
   return (
       <Switch>
-        <Route exact path ="/" component={ Header}/>
+        <Route exact path ="/" component={ HomePage}/>
         <Route  exact path ="/about" component={ About}/>
         <Route  exact  path ="/contact" component={ Contact}/>
       </Switch>
