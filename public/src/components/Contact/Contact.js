92e6bf77466b783cc41a1e609f00d12a0876c54b
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2020-01-13 14:09:20 +0530

    contact component

--- public/src/components/Contact/Contact.js
+++ public/src/components/Contact/Contact.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Header from '../Header/Header'
 
 export default class Contact extends Component {
     render() {
         console.log("contact");
         return (
             <div>
+                <Header/>
                 <h1>Contact</h1>
             </div>
         )
