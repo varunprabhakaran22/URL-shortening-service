c61607e5cab990f0082bfa3cba9f00754db85ff1
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2020-01-13 22:42:03 +0530

    connecting the server to the mongodb

--- Server/index.js
+++ Server/index.js
@@ -3,20 +3,26 @@ const express = require('express');
 const bodyparser = require('body-parser')
 const mongoose = require('mongoose');
 const cors=require('cors');
-const port = process.env.PORT || 8000;
+const connectDB = require('../Server/config/db')
 
+const port = process.env.PORT || 8000;
 const app = express();
-app.use(bodyparser.urlencoded({encoded:false}))
 app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({encoded:false}))
 app.use(cors());
 
+//@ connecting to db
+connectDB()
 
+
+
+//importing the files
 const urlShortenning = require('../Server/routes/urlShortening')
 
+//Redirecting to routes
 app.use('/api/url', urlShortenning)
 
 // Server can be accessed from port 8000
 app.listen(port, () => {
     console.log('Server is ready');
 })
- 
