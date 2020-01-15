d20c22c9af0d1cd5ef1bbb1cc5d06556b17884ad
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2020-01-15 08:06:21 +0530

    dynamically rendering the text box to show case the short url

--- public/src/components/HomePage/HomePage.js
+++ public/src/components/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Header from '../Header/Header'
+import LandingPage from '../LandingPage/LandingPage'
 import axios from 'axios'
 import '../HomePage/HomePage.css'
 
@@ -9,7 +10,8 @@ class HomePage extends Component {
         this.state ={
             longUrl:"",
             shortUrl:"",
-            isLoading:false
+            isLoading:false,
+            isLogin:true
         }
     }
 
@@ -26,6 +28,7 @@ class HomePage extends Component {
         })
         let url = {}
         url.longUrl = this.state.longUrl
+        url.token = sessionStorage.getItem("token");
         console.log("executing");
         axios.post("http://localhost:5000/api/url/shorten",url)
         .then((res,req) =>{
@@ -35,14 +38,28 @@ class HomePage extends Component {
                 isLoading:!this.state.isLoading
             })
         })
-        .catch((err) => console.log(err))
-        // this.props.history.push("/")
+        .catch((err) => {
+            console.log(err)
+            this.setState({
+                isLogin:false,
+                isLoading:!this.state.isLoading
+            })
+            console.log("loginn first");
+        })
     }
 
     render() {
         return (
             <div>
-                < Header/>
+                {this.state.isLogin?
+                    (
+                        < Header/>
+                    )
+                    :
+                    (
+                        <LandingPage />
+                    )
+                }
                 { this.state.isLoading ?
                     (
                         <h1 className = "loading">....Loading </h1>
@@ -55,13 +72,30 @@ class HomePage extends Component {
                                     className="frmField" 
                                     placeholder="Long Url" 
                                     name = "longUrl"  
-                                    value={this.state.source} 
+                                    autoComplete = "off"
+                                    value={this.state.longUrl} 
                                     onChange = {event => this.handleChange(event)}
                                 />
                                 <button type="button" className = "frmBtn"  onClick = {this.handleSubmit}> Url Shorten </button>
                             </form>
                         </div>
                     )
+                }
+
+                { this.state.shortUrl.length > 0 ?
+                    (
+                        <div>
+                            <label className="shorturl"> Short Url </label>
+                            <input type="text" 
+                                className="frmField" 
+                                name = "shortUrl"  
+                                value = {this.state.shortUrl}
+                                readOnly={this.state.shortUrl}
+                            />
+                        </div>
+                    )
+                    :
+                    (null)
                 }
             </div>
         )
