600ae6e3122c0ab3b34e31822a0967d283416376
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2020-01-15 08:04:34 +0530

    importing all the components and defining the routes for the components

--- public/src/App.js
+++ public/src/App.js
@@ -4,15 +4,20 @@ import './App.css';
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
 import HomePage from './components/HomePage/HomePage'
+import Login from './components/Login/Login'
+import LandingPage from './components/LandingPage/LandingPage'
+import CreateUser from './components/CreateUser/CreateUser'
 
 function App() {
   return (
       <Switch>
-        <Route exact path ="/" component={ HomePage}/>
+        <Route exact path ="/" component={ LandingPage }/>
+        <Route exact path ="/loginPage" component={ Login }/>
+        <Route exact path ="/createUser" component={ CreateUser }/>
+        <Route exact path ="/homepage" component={ HomePage}/>
         <Route  exact path ="/about" component={ About}/>
         <Route  exact  path ="/contact" component={ Contact}/>
       </Switch>
-    
   );
 }
 
