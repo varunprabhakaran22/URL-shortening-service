d6a48f4e92c56f981782c61dcd61205299725587
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2020-01-22 00:42:16 +0530

    updating the routes based on heroku req

--- Server/index.js
+++ Server/index.js
@@ -15,22 +15,14 @@ connectDB();
 
 app.use(express.json());
 
+
 // Define Routes
 app.use('/', require('./routes/index'));
 app.use('/api/auth', require('./routes/login'));
 app.use('/api/url', require('./routes/url')); 
 
 
-if (process.env.NODE_ENV === 'production') {
 
-    // Set static folder
-    app.use(express.static('public/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html'));
-    });
-  }
-  
-  const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
   
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
